d0c515108b0d164775c8ba2523b261c6d0496ae4
Rafaela Amboni Ghellere <rafaelaaghellere@gmail.com>
2026-08-11 21:20:38 -0300

    Update ex01.cpp

--- 2 fase/resolução de problemas estruturados I/ex01.cpp
+++ 2 fase/resolução de problemas estruturados I/ex01.cpp
@@ -1,1 +1,26 @@
+#include<stdio.h>
 
+main()
+{
+	int posicoes, i, aux;
+	printf("Informe quantas posicoes no vetor: ");
+	scanf("%i",&posicoes);
+	
+	int A[posicoes];
+	
+	printf("Vetor A\n");
+	for (i = 0; i<posicoes; i++)
+	{
+		printf("Informe um numero: ");
+		scanf("%i",&A[i]);
+	}
+	
+	aux = A[posicoes - 3];
+	A[posicoes - 3] = A[posicoes - 2];
+	A[posicoes - 2] = aux;
+	
+	for (i=0;i<posicoes;i++)
+	{
+		printf("%i\t",A[i]);
+	}
+}
